bba9c125501b6cc2251a2abaedb3263f9ee739a4
HanabishiRecca <HanabishiRecca@users.noreply.github.com>
2018-09-09 22:39:07 +0500

    Add project files.

---
+++ InwallCoolersAndVents/InwallCoolersAndVents.cs
@@ -0,0 +1,7 @@
+﻿using Verse;
+
+public class PlaceWorker_WallRestriction : PlaceWorker {
+    public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore) {
+        return ((loc.GetEdifice(map)?.def?.graphicData?.linkFlags & (LinkFlags.Rock | LinkFlags.Wall)) > 0) ? AcceptanceReport.WasAccepted : "MessagePlaceOnlyOnTheWall".Translate();
+    }
+}
---
+++ InwallCoolersAndVents/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("In-wall coolers and vents")]
+[assembly: AssemblyProduct("[RimWorld mod] In-wall coolers and vents")]
+[assembly: AssemblyCopyright("Hanabishi")]
+[assembly: AssemblyVersion("1.0.*")]
